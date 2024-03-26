2b2b5c237e1d474bcde22f4ecbb522f940b189de
Jens Pots <jens@jenspots.com>
2024-03-26 20:06:19 +0100

    test: initial config

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-function log(incoming: string, outgoing: string): void {
-    console.log(incoming);
+import { Stream, Writer } from "@treecg/connector-types";
+
+export function log(
+    incoming: Stream<string>,
+    outgoing: Writer<string>,
+): () => Promise<void> {
+    return async () => {
+        incoming.on("data", (data) => {
+            console.log(data);
+            outgoing.push(data);
+        });
+
+        incoming.on("end", () => {
+            outgoing.end();
+        });
+    };
 }
